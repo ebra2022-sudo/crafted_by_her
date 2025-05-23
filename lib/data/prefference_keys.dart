8dd3def1e9b2b838fa47d6d90c3d2959881bf7b2
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-05-23 04:03:26 +0300

    implement implement shared prefernce key enum class

---
+++ lib/data/prefference_keys.dart
@@ -0,0 +1,6 @@
+class PreferencesKeys {
+  static const String userKey = 'user';
+  static const String isLoggedInKey = 'is_logged_in';
+  static const String tokenKey = 'user_token';
+  static const String profileImagePathKey = 'profile_image_path';
+}
